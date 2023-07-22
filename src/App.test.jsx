664644b334d05475796903a014bcbe842e12d67f
Samuel Brito <sambriton7@gmail.com>
2023-07-21 21:55:34 -0300

    Trying to do some tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,19 @@
 /** @jest-environment jsdom */
+import '@testing-library/jest-dom'
 import { renderTheme } from "./style/renderTheme"
 import { screen } from '@testing-library/react';
 import App from "./App"
 import "jest-styled-components";
 import { theme } from "./style/theme";
-import '@testing-library/jest-dom'
+
 
 test('should render', () => {
   const { debug } = renderTheme(<App />)
-  const headingContainer = screen.getByRole('heading', { name: 'Olá!' }).parentElement
-  expect(headingContainer).toHaveStyle({
+  debug()
+  const headingContainer = screen.getByRole('heading', { name: 'Olá!' }).parentElement;
 
+  expect(headingContainer).toHaveStyle({
     background: theme.colors.mainBg,
   });
-  expect(headingContainer).toHaveStyleRule('background', 'red');
+  expect(headingContainer).toHaveStyleRule('background', 'red')
 });
