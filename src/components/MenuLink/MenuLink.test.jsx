56ca5e6eb85ad4de803bf74974de209eb5196ae3
Samuel Brito <sambriton7@gmail.com>
2023-08-02 19:40:08 -0300

    Creating a NavBar component

--- src/components/MenuLink/MenuLink.test.jsx
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -3,7 +3,10 @@ import { MenuLink } from '.'
 import { renderTheme } from '../../templates/styles/renderTheme'
 describe('<MenuLink/>', () => {
   it('should render', () => {
-    renderTheme(<MenuLink>children</MenuLink>)
-    expect(screen.getByRole('heading')).toBeInTheDocument()
+    renderTheme(<MenuLink link='http://localhost'>children</MenuLink>)
+    expect(screen.getByRole('link', { name: 'children' })).toHaveAttribute(
+      'target',
+      '_self'
+    )
   })
 })
