69108bd051acfea2cef9695662c48c7d9b7e5d39
Samuel Brito <sambriton7@gmail.com>
2023-08-03 11:48:15 -0300

    Finishing the NavLink tests

--- src/components/NavLinks/NavLinks.test.jsx
+++ src/components/NavLinks/NavLinks.test.jsx
@@ -1,9 +1,32 @@
 import { screen } from '@testing-library/react'
 import { NavLinks } from '.'
 import { renderTheme } from '../../templates/styles/renderTheme'
-describe('<NavLinks/>', () => {
-  it('should render', () => {
-    renderTheme(<NavLinks>children</NavLinks>)
-    expect(screen.getByRole('heading')).toBeInTheDocument()
-  })
-})
+import { theme } from '../../templates/styles/theme'
+
+import mock from './mock'
+
+describe('<NavLinks />', () => {
+  it('should render links', () => {
+    renderTheme(<NavLinks links={mock} />);
+    expect(screen.getAllByRole('link')).toHaveLength(mock.length);
+  });
+
+  it('should not render links', () => {
+    renderTheme(<NavLinks />);
+    expect(screen.queryAllByText(/links/i)).toHaveLength(0);
+  });
+
+  it('should render links', () => {
+    renderTheme(<NavLinks links={mock} />);
+    screen.debug(screen.getByText(/link 10/i).parentElement)
+    expect(screen.getByText(/link 10/i).parentElement).toHaveStyleRule(
+      "flex-flow: column wrap",
+
+    );
+  });
+
+  it('should match snapshot', () => {
+    renderTheme(<NavLinks links={mock} />);
+    expect(screen.getAllByRole('link')).toMatchSnapshot();
+  });
+});
