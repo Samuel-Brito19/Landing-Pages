719c62cc51f93989f373333e26f351d29204a49b
Samuel Brito <sambriton7@gmail.com>
2023-08-08 09:17:53 -0300

    Making a style Container in GridTwoColum

--- src/components/GridTwoColum/styles.js
+++ src/components/GridTwoColum/styles.js
@@ -1,4 +1,22 @@
 import styled, { css } from 'styled-components'
+
 export const Container = styled.div`
+${({ theme }) => css`
+display: grid;
+grid-template-columns: 1fr 2fr;
+align-items: center;
+gap: ${theme.spacings.large};
+`}
+`;
+
+export const TextContainer = styled.div`
+${({ theme }) => css``}
+`;
+
+export const ImageContainer = styled.div`
+${({ theme }) => css``}
+`;
+
+export const Image = styled.img`
 ${({ theme }) => css``}
 `;
--- src/templates/App/App.jsx
+++ src/templates/App/App.jsx
@@ -1,14 +1,14 @@
-import { Heading } from '../../components/Heading';
 import * as Styled from './styles';
-import { NavLinks } from '../../components/NavLinks';
-import { Menu } from '../../components/Menu';
 import linksMock from '../../components/NavLinks/mock'
+import { GridTwoColum } from '../../components/GridTwoColum';
 
 
 function Home() {
   return (
     <div className="App">
-      <Menu />
+      <GridTwoColum title={'Bora tomar uma'}
+        text={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`}
+        srcImg={'assets/images/javascript.svg'} />
     </div>
   );
 }
