f29456151972feb2bf919e0daa24945b575eff55
Samuel Brito <sambriton7@gmail.com>
2023-08-09 16:18:46 -0300

    Creating a mock archive

--- src/components/GridSection/index.jsx
+++ src/components/GridSection/index.jsx
@@ -2,19 +2,33 @@ import P from 'prop-types'
 import * as Styled from './styles'
 import { SectionBackground } from '../SectionBackground'
 import { Heading } from '../Heading'
+import { TextComponent } from '../TextComponent'
 
-export const GridSection = ({ title }) => {
+export const GridSection = ({ title, description, background = false, grid }) => {
   return (
     <SectionBackground>
       <Styled.Container>
-        <Heading>
-          {title}
-        </Heading>
+        <Heading size='huge' upperCase>{title}</Heading>
+        <TextComponent>{description}</TextComponent>
+        <Styled.Grid>{grid.map(el => (
+          <Styled.GridElement key={el.title}>
+            <Heading size='medium'>{el.title}</Heading>
+            <TextComponent>{description}</TextComponent>
+          </Styled.GridElement>
+        ))}</Styled.Grid>
       </Styled.Container>
     </SectionBackground>
 
   )
 }
 GridSection.propTypes = {
-  title: P.string.isRequired
+  title: P.string.isRequired,
+  description: P.string.isRequired,
+  background: P.bool,
+  grid: P.arrayOf(
+    P.shape({
+      title: P.string.isRequired,
+      description: P.string.isRequired,
+    }),
+  ).isRequired,
 }
