4ebd2c0e5e007ca672a854d968357b8da232f5a4
Samuel Brito <sambriton7@gmail.com>
2023-08-15 20:44:39 -0300

    Finishing tests in the map-menu

--- src/api/map-menu.js
+++ src/api/map-menu.js
@@ -1,5 +1,34 @@
-export const mapMenu = () => {
+export const mapMenu = (menu = {}) => {
+  const {
+    open_in_new_tab: newTab = false,
+    logo_text: text = '',
+    logo_link: link = '',
+    logo: { url: srcImg = '' } = '',
+    menu: links = [],
+
+  } = menu
   return {
-    newTab: false
+    newTab,
+    text,
+    link,
+    srcImg,
+    links: mapManuLinks(links),
   }
+}
+
+export const mapManuLinks = (links = []) => {
+  return links.map(item => {
+    const {
+      open_in_new_tab: newTab,
+      link_text: children,
+      url: link = '',
+    } = item
+
+    return {
+      newTab,
+      children,
+      link,
+
+    }
+  })
 }
