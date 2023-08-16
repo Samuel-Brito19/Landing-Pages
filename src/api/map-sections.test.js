db5a89d0a6cae3cb1cd2bfda33e61938712a7448
Samuel Brito <sambriton7@gmail.com>
2023-08-16 10:23:31 -0300

    Finishing tests in the map-sections-grid-text

--- src/api/map-sections.test.js
+++ src/api/map-sections.test.js
@@ -1,4 +1,4 @@
-import { mapSectionContent, mapSectionTwoColumns } from "./map-sections"
+import { mapSectionContent, mapSectionTwoColumns, mapTextGrid } from "./map-sections"
 
 describe('map-sections', () => {
 
@@ -88,7 +88,61 @@ describe('map-sections', () => {
     expect(data.component).toBe('section.section-content');
     expect(data.sectionId).toBe('pricing');
     expect(data.title).toBe('Pricing');
-    expect(data.html).toBe('');
+    expect(data.html).toBe('abc');
   })
 
+  it('should map grid text', () => {
+    const data = mapTextGrid({
+      __component: 'section.section-grid',
+      _id: '602fdf2d540c00269e056174',
+      description: 'abc',
+      title: 'My Grid',
+      text_grid: [
+        {
+          _id: '602fdf2d540c00269e05617c',
+          title: 'Teste 1',
+          description: 'Coisa',
+          __v: 0,
+          id: '602fdf2d540c00269e05617c',
+        },
+        {
+          _id: '602fdf2d540c00269e05617d',
+          title: 'Teste 2',
+          description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis cum delectus molestias. Atque doloribus nobis laudantium esse ut, non commodi maxime distinctio veritatis unde, reprehenderit minus ad dolores provident maiores.',
+          __v: 0,
+          id: '602fdf2d540c00269e05617d',
+        },
+        {
+          _id: '602fdf2d540c00269e05617e',
+          title: 'Teste 3',
+          description:
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis cum delectus molestias. Atque doloribus nobis laudantium esse ut, non commodi maxime distinctio veritatis unde, reprehenderit minus ad dolores provident maiores.',
+          __v: 0,
+          id: '602fdf2d540c00269e05617e',
+        },
+      ],
+      image_grid: [],
+      metadata: {
+        background: true,
+        _id: '602fdf2e540c00269e056199',
+        name: 'grid-one',
+        section_id: 'grid-one',
+        __v: 0,
+        id: '602fdf2e540c00269e056199',
+      },
+      __v: 2,
+      id: '602fdf2d540c00269e056174',
+    });
+    expect(data.background).toBe(true);
+    expect(data.component).toBe('section.section-grid');
+    expect(data.sectionId).toBe('grid-one');
+    expect(data.title).toBe('My Grid');
+    expect(data.description).toBe('');
+    expect(data.grid[0].title).toBe('Teste 1');
+  });
+
 })
+
+
+
