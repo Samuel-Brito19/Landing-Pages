79cecfdcc5fa0a6b10480cea3c43fd95d3ec0533
Samuel Brito <sambriton7@gmail.com>
2023-08-17 18:46:58 -0300

    Trying to catch the api data

--- src/templates/App/App.jsx
+++ src/templates/App/App.jsx
@@ -2,13 +2,43 @@ import { GridImage } from '../../components/GridImage';
 import * as Styled from './styles';
 import { mockBase } from '../Base/mock'
 import { Base } from '../Base';
-
-
+import { useEffect, useState } from 'react';
+import { mapData } from '../../api/map-data'
 
 
 function Home() {
-  return (
-    <Base {...mockBase} />
-  );
+  const [data, setData] = useState([]);
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    const load = async () => {
+      console.log('fetching');
+      const data = await fetch(
+        'http://localhost:1337/api/pages/?filters[slug]=landing-page&populate=deep',
+      );
+      const json = await data.json();
+      const { attributes } = json.data[0];
+      const pageData = mapData([attributes]);
+      setData(() => pageData[0]);
+    };
+
+    if (isMounted.current === true) {
+      load();
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  if (data === undefined) {
+    return <h1>Página não encontrada</h1>;
+  }
+
+  if (data && !data.slug) {
+    return <h1>Carregando...</h1>;
+  }
+
+  return <Base {...mockBase} />;
 }
 export default Home;
